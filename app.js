6723f8d9ef6e9be9c9e5adee12178f2250ad4f9a
Eliza Moore <elzmoore13@gmail.com>
2018-06-11 01:58:13 +0000

    add node-blog exercise

---
+++ app.js
@@ -0,0 +1,79 @@
+/* app.js */
+
+//require and instantiate express
+const app = require('express')()
+
+//fake posts to simulate a database
+const posts = [
+  {
+    id: 1,
+    author: 'ELIZA',
+    title: "Being Awesome",
+    body: 'Blog post number 1'
+  },
+  {
+    id: 2,
+    author: 'Drake',
+    title: "Growing Up at Degrassi",
+    body: 'Blog post number 2'
+  },
+  {
+    id: 3,
+    author: 'John',
+    title: "This is a Title",
+    body: 'Blog post number 3'
+  },
+  {
+    id: 4,
+    author: 'Santa',
+    title: "The Nice List",
+    body: 'Blog post number 4'
+  }
+
+]
+
+//set the view engine to ejs
+app.set('view engine', 'ejs')
+
+//blog home page
+app.get('/', (req, res) => {
+  //render 'home.ejs' with the list of posts
+  res.render('home', {posts: posts})
+})
+
+//blog post
+app.get('/post/:id', (req, res) => {
+  //find the post in the 'posts' array
+  const post = posts.filter((post) => {
+    return post.id == req.params.id
+  })[0]
+
+  //render the 'posts.ejs' template with the post content
+  res.render('post', {
+    author: post.author,
+    title: post.title,
+    body: post.body,
+  })
+})
+
+app.listen(8080)
+
+console.log('listening on port 8080');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
